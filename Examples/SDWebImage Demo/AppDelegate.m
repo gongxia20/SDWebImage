37d57305b6285c7d6c0e50e535f83eb6a847cf9b
James <gongxia20@sina.cn>
2018-03-30 17:14:46 +0800

    在didFinishLaunchingWithOptions里面加了注释

--- Examples/SDWebImage Demo/AppDelegate.m
+++ Examples/SDWebImage Demo/AppDelegate.m
@@ -19,13 +19,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    //Add a custom read-only cache path
+    // 添加一个自定义的只读缓存路径 (SD缓存里面就有两个缓存路劲了,一个是默认的一个是自己添加的)
     NSString *bundledPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"CustomPathImages"];
     [[SDImageCache sharedImageCache] addReadOnlyCachePath:bundledPath];
 
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-
+    
+    // 应用程序启动后自定义的覆盖点.
     MasterViewController *masterViewController = [[MasterViewController alloc] initWithNibName:@"MasterViewController" bundle:nil];
     self.navigationController = [[UINavigationController alloc] initWithRootViewController:masterViewController];
     self.window.rootViewController = self.navigationController;
